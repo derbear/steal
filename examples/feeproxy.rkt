a987dfba0b5c7bd1b932d9a1eb4d3acd5590792e
Derek Leung <derek@algorand.com>
2019-09-16 16:46:21 -0400

    Fix some transaction-replay bugs. (Also gtxn instead of txnN.)

--- examples/feeproxy.rkt
+++ examples/feeproxy.rkt
@@ -1,9 +1,17 @@
-(define feeproxy
-  '(&& (= (txn closeremainderto) "Alice")
-       (= (txn receiver) "Alice")
-       (ed25519verify arg0 "X" "Bob")))
+;; TODO will need to reason about the fee over time...
+;; (define feeproxy
+;;   '(&& (= (txn closeremainderto) "Alice")
+;;        (= (txn receiver) "Alice")
+;;        (= (len (txn note)) 0)
+;;        (= 500 (- (txn firstvalid) (txn lastvalid)))
+;;        (ed25519verify "X" (txn firstvalid) "Bob")))
 
 (define feeproxykeyreg
   '(&& (= (txn closeremainderto) "Zero")
        (= (txn receiver) "Zero")
-       (ed25519verify arg0 "X" "Bob")))
+       (= (txn amount) 0)
+       (= (txn fee) 1,000)
+       (= (len (txn note)) 0)
+       (= (txn lastvalid) (+ 500 (txn firstvalid)))
+       (= (% (txn firstvalid) 10,000) 0)
+       (ed25519verify (txn firstvalid) "Bob" "X")))
